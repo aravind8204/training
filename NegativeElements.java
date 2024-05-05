c42167fc195618d055b4d6fe2be8360439c3459d
Aravindhan V <70193493+aravind8204@users.noreply.github.com>
2024-05-05 23:43:35 +0530

    Create NegativeElements.java

---
+++ NegativeElements.java
@@ -0,0 +1,10 @@
+class NegativeElements{
+public static void main(String args[]){
+int a[]={4,5,3,-3,0,-3,1,-3,-1,-6,-9,1};
+  for(int i=0;i<a.lenght;i++){
+    if(a[i]<0){
+      System.out.println(a[i]);
+    }
+  }
+}
+}
