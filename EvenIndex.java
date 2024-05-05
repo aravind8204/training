872061b3a7d76b15881afc9e6d89807f2d04a88b
Aravindhan V <70193493+aravind8204@users.noreply.github.com>
2024-05-05 23:47:23 +0530

    Create EvenIndex.java

---
+++ EvenIndex.java
@@ -0,0 +1,12 @@
+class EvenIndex{
+public static void main(String args[]){
+  int a[]={2,1,3,4,5,2,5,24,24,2,1,0};
+  int sum=0;
+  for(int i=0;i<a.length;i++){
+    if(i%2==0){
+      sum=sum+i;
+    }
+  }
+  System.out.println(sum);
+}
+}
